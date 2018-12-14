ef5588823a0ed4064ec2c1ac12e416ed6d92beaf
Rachid Al Maach <rachid@qraft.nl>
2018-12-14 22:11:06 +0100

    Bump elixir version

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule ExOneSignal.Mixfile do
       description: "A simple interface to interact with OneSignal's push notification API.",
       package: package(),
       source_url: "https://github.com/logit-ai/ex_one_signal",
-      elixir: "~> 1.5.1",
+      elixir: "~> 1.7.3",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
